caff35115ae450d7879cb703b9795929269f6f93
Ben Clauser Ringia <benringia@gmail.com>
2020-07-12 14:56:45 +0800

    Update auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const BASEURL = 'http://localhost:3000/api/chatup';
+const BASEURL = 'https://chatup1.herokuapp.com';
 
 @Injectable({
   providedIn: 'root'
